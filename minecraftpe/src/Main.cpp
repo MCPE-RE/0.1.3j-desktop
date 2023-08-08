08d78f413e1e1a1c2d82ed5e1a2882405c5c7783
MFDGaming <alexandros.argentakis@gmail.com>
2023-08-08 13:24:37 +0300

    Added some keyboard inputs

--- minecraftpe/src/Main.cpp
+++ minecraftpe/src/Main.cpp
@@ -6,8 +6,34 @@
 #include "../../0.1.3j-core/src/client/gui/Font.h"
 #include "../../0.1.3j-core/src/client/gui/GuiComponent.h"
 #include "../../0.1.3j-core/src/NinecraftApp.h"
+#include "../../0.1.3j-core/src/client/input/keyboard/Keyboard.h"
 #include "AppPlatform_linux.h"
 
+NinecraftApp *app;
+
+void keyboard_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ENTER || key == GLFW_KEY_KP_ENTER) {
+        if (action == GLFW_PRESS) {
+            Keyboard::feed(app->options.keyMenuOk.keyCode, 1);
+        } else if (action == GLFW_RELEASE) {
+            Keyboard::feed(app->options.keyMenuOk.keyCode, 0);
+        }
+    }
+    if (key == GLFW_KEY_UP) {
+        if (action == GLFW_PRESS) {
+            Keyboard::feed(app->options.keyMenuPrevious.keyCode, 1);
+        } else if (action == GLFW_RELEASE) {
+            Keyboard::feed(app->options.keyMenuPrevious.keyCode, 0);
+        }
+    }
+    if (key == GLFW_KEY_DOWN) {
+        if (action == GLFW_PRESS) {
+            Keyboard::feed(app->options.keyMenuNext.keyCode, 1);
+        } else if (action == GLFW_RELEASE) {
+            Keyboard::feed(app->options.keyMenuNext.keyCode, 0);
+        }
+    }
+}
 
 int main() {
     if (!glfwInit()) {
@@ -17,11 +43,7 @@ int main() {
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_API);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-#if defined(_WIN32) || defined(__APPLE__)
     glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
-#else
-    glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_EGL_CONTEXT_API);
-#endif
 
     window = glfwCreateWindow(720, 480, "Minecraft: Pocket Edition", NULL, NULL);
     if (!window) {
@@ -40,10 +62,12 @@ int main() {
 
     AppContext context = {NULL, NULL, NULL, &platform, false};
 
-    NinecraftApp *app = new NinecraftApp();
+    app = new NinecraftApp();
     app->init(&context);
 
     app->setSize(720, 480);
+
+    glfwSetKeyCallback(window, keyboard_callback);
 
     while (!glfwWindowShouldClose(window)) {
         app->update();
